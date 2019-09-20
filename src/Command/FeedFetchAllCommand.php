ca5fd9198a43b8d40327a6ada51262ef270de57c
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-09-20 15:00:15 +0100

    Feed item async fetching

--- src/Command/FeedFetchAllCommand.php
+++ src/Command/FeedFetchAllCommand.php
@@ -3,36 +3,39 @@
 namespace App\Command;
 
 use andreskrey\Readability\Readability;
+use App\Entity\Feed;
 use App\Entity\FeedItem;
 use App\Repository\FeedItemRepository;
 use App\Repository\FeedRepository;
+use FeedIo\Feed\ItemInterface as RawFeedItem;
 use FeedIo\FeedIo;
 use GuzzleHttp\Client;
+use GuzzleHttp\Promise\Promise;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\OutputInterface as CommandOutput;
+use function GuzzleHttp\Promise\settle;
 
 class FeedFetchAllCommand extends Command
 {
+    private const FEED_ITEM_BATCH_SIZE = 20;
+
     /** @var \FeedIo\FeedIo */
     private $feedIo;
-    /**
-     * @var \App\Repository\FeedRepository
-     */
+
+    /** @var \App\Repository\FeedRepository */
     private $feedRepository;
-    /**
-     * @var \andreskrey\Readability\Readability
-     */
+
+    /** @var \andreskrey\Readability\Readability */
     private $readability;
-    /**
-     * @var \GuzzleHttp\Client
-     */
+
+    /** @var \GuzzleHttp\Client */
     private $guzzle;
-    /**
-     * @var \App\Repository\FeedItemRepository
-     */
+
+    /** @var \App\Repository\FeedItemRepository */
     private $feedItemRepository;
 
     public function __construct(
@@ -61,7 +64,7 @@ class FeedFetchAllCommand extends Command
             ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, CommandOutput $output)
     {
         $feeds    = $this->feedRepository->findAll();
         $numFeeds = count($feeds);
@@ -78,35 +81,81 @@ class FeedFetchAllCommand extends Command
             $this->feedRepository->save($feed);
 
             $numItems = count($feedContents);
-            $output->writeln(sprintf('Found %s feed items', $numItems));
+            $output->writeln(sprintf('Found %s feed items.', $numItems));
+
+            $promises     = [];
+            $rawFeedItems = [];
 
             foreach ($feedContents as $feedItemKey => $rawFeedItem) {
-                /** @var \FeedIo\Feed\ItemInterface $rawFeedItem */
+                $currentItemNumber = $feedItemKey + 1;
+
+                /** @var RawFeedItem $rawFeedItem */
                 $output->writeln(sprintf(
-                    'Processing %s of %s: %s',
-                    ($feedItemKey + 1),
+                    'Acquiring %s of %s: %s',
+                    $currentItemNumber,
                     $numItems,
                     $rawFeedItem->getTitle()
                 ));
 
-                $rawContents = $this->guzzle->get($rawFeedItem->getLink())->getBody()->getContents();
+                $promises[$rawFeedItem->getLink()]     = $this->guzzle->getAsync($rawFeedItem->getLink());
+                $rawFeedItems[$rawFeedItem->getLink()] = $rawFeedItem;
 
-                $feedItem = $this->feedItemRepository->findOneBy(['link' => $rawFeedItem->getLink()]);
-                if ($feedItem === null) {
-                    $feedItem = new FeedItem();
+                if (($currentItemNumber % self::FEED_ITEM_BATCH_SIZE === 0) || $currentItemNumber === $numItems) {
+                    $this->processBatch($promises, $rawFeedItems, $feed, $output);
+
+                    $promises     = [];
+                    $rawFeedItems = [];
                 }
-
-                $feedItem
-                    ->setFeed($feed)
-                    ->setTitle($rawFeedItem->getTitle())
-                    ->setDescription($rawContents)
-                    ->setLink($rawFeedItem->getLink())
-                    ->setLastModified($rawFeedItem->getLastModified());
-
-                $this->feedItemRepository->save($feedItem);
             }
         }
 
         $output->writeln('Finished.');
+    }
+
+    /**
+     * @param Promise[]     $promises
+     * @param RawFeedItem[] $rawFeedItems
+     * @param Feed          $feed
+     * @param CommandOutput $output
+     */
+    private function processBatch(array $promises, array $rawFeedItems, Feed $feed, CommandOutput $output): void
+    {
+        $numRawFeedItems = count($rawFeedItems);
+
+        $output->writeln(sprintf('Finalising acquisition of %s feed items', $numRawFeedItems));
+
+        // This ignores any errors fetching the item
+        $fetchedSuccessfully = settle($promises)->wait();
+
+        $counter = 1;
+        foreach ($fetchedSuccessfully as $link => $promiseResult) {
+            /** @var RawFeedItem $rawFeedItem */
+            $rawFeedItem = $rawFeedItems[$link];
+
+            $output->writeln(sprintf('Processing %s of %s: %s', $counter, $numRawFeedItems, $rawFeedItem->getTitle()));
+
+            /** @var ResponseInterface $response */
+            $response = $promiseResult['value'];
+
+            $rawContents = $response->getBody()->getContents();
+
+            $feedItem = $this->feedItemRepository->findOneBy(['link' => $rawFeedItem->getLink()]);
+            if ($feedItem === null) {
+                $feedItem = new FeedItem();
+            }
+
+            $feedItem
+                ->setFeed($feed)
+                ->setTitle($rawFeedItem->getTitle())
+                ->setDescription($rawContents)
+                ->setLink($rawFeedItem->getLink())
+                ->setLastModified($rawFeedItem->getLastModified());
+
+            $this->feedItemRepository->save($feedItem);
+
+            $counter++;
+        }
+
+        $output->writeln('Batch finalised.');
     }
 }

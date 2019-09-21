9ba8feb48ac40263a04dcb9ec808bb7690f48611
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2019-09-21 13:36:17 +0100

    Add a bunch of example feeds as fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,7 +1,9 @@
 <?php
+declare(strict_types=1);
 
 namespace App\DataFixtures;
 
+use App\Entity\Feed;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +11,21 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $feeds = [
+            'http://feeds.arstechnica.com/arstechnica/index',
+            'http://rss.slashdot.org/Slashdot/slashdotMainatom',
+            'http://feeds.bbci.co.uk/news/rss.xml',
+            'https://www.sciencedaily.com/rss/top.xml',
+            'https://www.sciencedaily.com/rss/space_time.xml',
+            'https://www.sciencedaily.com/rss/matter_energy.xml',
+            'https://www.sciencedaily.com/rss/computers_math.xml',
+            'https://www.universetoday.com/feed/',
+            'https://www.phoronix.com/rss.php',
+        ];
+
+        foreach ($feeds as $feed) {
+            $manager->persist((new Feed())->setFeedUrl($feed));
+        }
 
         $manager->flush();
     }

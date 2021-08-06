26332a8f29fcaaa4dd7c6e907e3b9c7140adb4fa
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2021-08-06 16:23:05 +0100

    Add validation rules to new user form

--- src/Form/RssUserType.php
+++ src/Form/RssUserType.php
@@ -9,22 +9,20 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RssUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-//            ->add(
-//                'roles',
-//                ChoiceType::class, [
-//                    'choices' => [
-//                        'ROLE_ADMIN',
-//                        'ROLE_USER',
-//                    ],
-//                ]
-//            )
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                ],
+            ])
             ->add('password', PasswordType::class);
     }
 
